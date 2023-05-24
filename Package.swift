72da71b00e1489bb33a28b924845401ee69d7929
Michael Eisemann <me@michaeleisemann.com>
2023-05-24 16:53:36 -0400

    I have no clue

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-nio", .upToNextMajor(from: "2.51.1")),
         .package(url: "https://github.com/apple/swift-nio-ssl", .upToNextMajor(from: "2.24.0")),
         .package(url: "https://github.com/apple/swift-nio-transport-services", .upToNextMajor(from: "1.17.0")),
-        .package(url: "https://github.com/awxkee/zstd.swift.git", .upToNextMajor(from: "1.0.1")),
+        .package(url: "https://github.com/awxkee/zstd.swift", .upToNextMajor(from: "1.0.1")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
